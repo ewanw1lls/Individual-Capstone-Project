ba2ad6d2e7b544381e099e899e732216a53ca4ff
Ewan Williams <ewandwill@gmail.com>
2025-03-10 15:18:32 +0000

    fix: correct secret key retrieval and update allowed hosts format

--- court_finder/settings.py
+++ court_finder/settings.py
@@ -26,12 +26,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'os.environ.get('SECRET_KEY')'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [court-finder-courts-0bc1e2d6a141.herokuapp.com]
+ALLOWED_HOSTS = ["court-finder-courts-0bc1e2d6a141.herokuapp.com"]
 
 
 # Application definition
