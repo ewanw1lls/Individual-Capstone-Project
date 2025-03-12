6da898377b591bec6c8398bea48071c273c7283f
Ewan Williams <ewandwill@gmail.com>
2025-03-12 14:37:14 +0000

    debug

--- courts/admin.py
+++ courts/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Court
 
 # Register your models here.
-admin.site.register(Post)
-
+admin.site.register(Court)
--- courts/models.py
+++ courts/models.py
@@ -1,21 +1,24 @@
 from django.db import models
 # from blog walkthrough v
 from django.contrib.auth.models import User   
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
-class Post(models.Model):
+class Court(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
-    User, on_delete=models.CASCADE, related_name="blog_posts"
+        User, on_delete=models.CASCADE, related_name="courts"
     )
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
-    rating = models.DecimalField(max_digits=3, decimal_places=1, null=True, blank=True)  # Rating from 0.0 to 10.0
+    rating = models.DecimalField(max_digits=3, decimal_places=1, null=True, blank=True, validators=[MinValueValidator(0.0), MaxValueValidator(10.0)])  # Rating from 0.0 to 10.0
     image = models.ImageField(upload_to='court_images/', null=True, blank=True)  # Court image
+    updated_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
+    
