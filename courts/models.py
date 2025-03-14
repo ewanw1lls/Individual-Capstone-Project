cad9ee6a7b2fb8d313e0923ab8b16fee2ca532c5
Ewan Williams <ewandwill@gmail.com>
2025-03-14 11:57:43 +0000

    add efit and delete reviews feature

--- courts/models.py
+++ courts/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 # from blog walkthrough v
-from django.contrib.auth.models import User   
+from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator, MaxValueValidator
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
+
 # Create your models here.
+
+
 class Court(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
@@ -15,17 +18,27 @@ class Court(models.Model):
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
-    rating = models.DecimalField(max_digits=3, decimal_places=1, null=True, blank=True, validators=[MinValueValidator(0.0), MaxValueValidator(10.0)])  # Rating from 0.0 to 10.0
-    image = models.ImageField(upload_to='court_images/', null=True, blank=True)  # Court image
+    rating = models.DecimalField(
+        max_digits=3, decimal_places=1,
+        null=True, blank=True,
+        validators=[
+            MinValueValidator(0.0),
+            MaxValueValidator(10.0)
+        ]
+    )  # Rating from 0.0 to 10.0
+    image = models.ImageField(
+        upload_to='court_images/', null=True, blank=True
+    )  # Court image
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
-    
+
     class Meta:
         ordering = ["-created_on"]
 
     def __str__(self):
         return f"{self.title} | written by {self.author}"
-    
+
+
 class Review(models.Model):
     Court = models.ForeignKey(
         Court, on_delete=models.CASCADE, related_name="reviews")
--- courts/urls.py
+++ courts/urls.py
@@ -4,4 +4,8 @@ from django.urls import path
 urlpatterns = [
     path('', views.CourtList.as_view(), name='home'),
     path('<slug:slug>/', views.court_detail, name='court_detail'),
+    path('<slug:slug>/edit_review/<int:review_id>',
+         views.review_edit, name='review_edit'),
+    path('<slug:slug>/delete_review/<int:review_id>',
+         views.review_delete, name='review_delete'),
 ]
