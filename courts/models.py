66529c2944847e68bae26210f7d47444fee8e0c3
Ewan Williams <ewandwill@gmail.com>
2025-03-17 15:32:13 +0000

    fix: preview image

--- courts/models.py
+++ courts/models.py
@@ -15,7 +15,6 @@ class Court(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="courts"
     )
-    profile_image = CloudinaryField('image', default='placeholder')
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
